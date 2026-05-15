02bf03270bbf774fa1fe778e9efa2eec3114a7e3
Rafael <rafcascos@gmail.com>
2026-05-15 16:49:43 -0300

    Correção em textos

--- src/main/pagamento/MaquinaLegado.java
+++ src/main/pagamento/MaquinaLegado.java
@@ -2,7 +2,7 @@ package main.pagamento;
 
 public class MaquinaLegado {
     public int executarTransacao(String tipo, long centavos) {
-        System.out.println("    [Maquineta] " + tipo + " de " + String.format("R$ %.2f", centavos / 100.0) + " → aprovado");
+        System.out.println("[Maquina] " + tipo + " de " + String.format("R$ %.2f", centavos / 100.0) + " → aprovado");
         return 0;
     }
 }
