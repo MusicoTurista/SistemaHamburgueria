1dbc9539a88e8303397643648b2f6afcbeee9492
Rafael <rafcascos@gmail.com>
2026-05-18 21:52:05 -0300

    Alteração em Loja

--- src/main/config/Loja.java
+++ src/main/config/Loja.java
@@ -1,13 +1,13 @@
 package main.config;
 
 public class Loja {
+    private Loja() {}
     private static Loja instance = new Loja();
+
     public final String nome = "Hamburgueria";
     public final double taxaServico = 0.10;
     public final int limiteCardapio = 50;
 
-    private Loja() {
-    }
 
     public static Loja getInstance() {
         return instance;
